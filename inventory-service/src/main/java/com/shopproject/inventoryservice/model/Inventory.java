c63f1e616a28a00e3e61818798b47f935d685d57
bchiramani <amanibchir00@gmail.com>
2023-11-16 15:21:02 +0100

    inventory service

---
+++ inventory-service/src/main/java/com/shopproject/inventoryservice/model/Inventory.java
@@ -0,0 +1,25 @@
+package com.shopproject.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//import jakarta.persistence.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
